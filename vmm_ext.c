33a5bcc003805616270996e6e98e9c6b009d7e0e
ricardo <klossricardo@gmail.com>
2014-10-19 15:57:20 -0200

    swap is here

--- vmm_ext.c
+++ vmm_ext.c
@@ -5,8 +5,16 @@
 #include <inttypes.h>
 #include <assert.h>
 
+#define NUMPAGES 256
+
 uint32_t freesStart;
 uint32_t freesEnd;
+
+uint32_t getFreeFrame(){
+	uint32_t freeFrame = freeStart;
+	freeStart = dccvmm_phy_read(freeFrame<<8 || 0);
+	return freeFrame;
+}
 
 void os_init(void){
 //set the free frames
@@ -24,16 +32,36 @@ void os_init(void){
 
 void os_alloc(uint32_t addr){
 	//search for a free frame
-	//update the frame behind to point to free frame next
+	//update the frame behind to point to next free frame
 	//make page table point to the found frame
 }
 void os_free(uint32_t addr){
 	//update the page table
-	// update the free list
+	//update the free list:
+	//	make the freed frame the freeStart and make it point to the old freeStart
 	//get the frame addr
 	
 }
 void os_swap(uint32_t pid){
+	//look for the frame of the proccess page table
+	// if not on the memory, load from disk
+	//call function to set the pagetable
+	int pFrame = (pid / 255);
+	int pRow = (pid % 255);
+	int i = 0;
+	uint32_t pdFrameAddr = 0;
+	for(i = procTableStart; i < pFrame; i++){
+		uint32_t nextFrame = dccvmm_phy_read(pdFrameAddr<<8);
+		if(pdFrameAddr == 0){
+			//not allocated
+			uint32_t newFrame = getFreeFrame();
+			dccvmm_phy_write(pdFrameAddr<<8, newFrame);
+			dccvmm_phy_write(newFrame<<8, 0);
+		}
+		pdFrameAddr = nextFrame;
+	}
+	uint32_t procPT = dccvmm_phy_read(pdFrameAddr<<8 | pRow);
+	dccvmm_set_page_table(dccvmm_phy_read());
 }
 
 uint32_t os_pagefault(uint32_t address, uint32_t perms, uint32_t pte){

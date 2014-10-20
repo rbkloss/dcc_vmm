fcfd48e68f64717caac40d1e98285f2676ec5d36
ricardo <klossricardo@gmail.com>
2014-10-19 22:38:50 -0200

    changed swap to a simpler way, before it could have a undetermined number of process, now it can have only 256, which is the minimum required in the specification of the assignment.

--- vmm_ext.c
+++ vmm_ext.c
@@ -7,25 +7,27 @@
 
 #define NUMPAGES 256
 
-uint32_t freesStart;
-uint32_t freesEnd;
+uint32_t freesStart_;
+uint32_t freesEnd_;
+uint32_t procTable_;
 
 uint32_t getFreeFrame(){
-	uint32_t freeFrame = freeStart;
-	freeStart = dccvmm_phy_read(freeFrame<<8 || 0);
+	uint32_t freeFrame = freeStart_;
+	freeStart = dccvmm_phy_read(freeFrame <<8 || 0);
 	return freeFrame;
 }
 
 void os_init(void){
 //set the free frames
-	freesStart = 1;
+	freesStart_ = 2;
+	procTable = 1;
 	int i = 0;
-	for(i = 0; i < NUMFRAMES; i++){
+	for(i = 2; i < NUMFRAMES; i++){
 		uint32_t addr = i << 8;
 		addr = addr || 1;
 		dccvmm_phy_write(addr, i +1);
 	}
-	freesEnd = NUMFRAMES - 1;
+	freesEnd_ = NUMFRAMES - 1;
 //set page table
 	dccvmm_set_page_table(0);
 }
@@ -43,25 +45,21 @@ void os_free(uint32_t addr){
 	
 }
 void os_swap(uint32_t pid){
-	//look for the frame of the proccess page table
+	//look for the frame of the process page table
 	// if not on the memory, load from disk
 	//call function to set the pagetable
-	int pFrame = (pid / 255);
-	int pRow = (pid % 255);
-	int i = 0;
-	uint32_t pdFrameAddr = 0;
-	for(i = procTableStart; i < pFrame; i++){
-		uint32_t nextFrame = dccvmm_phy_read(pdFrameAddr<<8);
-		if(pdFrameAddr == 0){
-			//not allocated
-			uint32_t newFrame = getFreeFrame();
-			dccvmm_phy_write(pdFrameAddr<<8, newFrame);
-			dccvmm_phy_write(newFrame<<8, 0);
+	uint32_t procPT = dccvmm_phy_read(procTable_ << 8 | pid);
+	if(procPT & PTE_INMEM){
+		dccvmm_set_page_table(dccvmm_phy_read());
+	}else{
+		if(procPT & PTE_VALID){
+		//process exists but is in the disk
+			//TODO
+		}else{
+			procPT = getFreeFrame();
+			dccvmm_phy_write(procTable_ << 8 | pid, procPT);
 		}
-		pdFrameAddr = nextFrame;
 	}
-	uint32_t procPT = dccvmm_phy_read(pdFrameAddr<<8 | pRow);
-	dccvmm_set_page_table(dccvmm_phy_read());
 }
 
 uint32_t os_pagefault(uint32_t address, uint32_t perms, uint32_t pte){

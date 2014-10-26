fd5cabb64a5e945ff34783123f3a928fd4615392
rbk <klossricardo@gmail.com>
2014-10-25 22:13:30 -0200

    Part 3 is Complete
    alloc now:
        check if the address pointed by pagedir is valid an allocates a frame for it if not.
        check if address is multiple of frame size.
    free now:
        clean the valid flags

--- vmm_ext.c
+++ vmm_ext.c
@@ -15,7 +15,7 @@ unsigned currentPID_;
 
 uint32_t getFreeFrame() {
     uint32_t freeFrame = freesStart_;
-    freesStart_ = dccvmm_phy_read(freeFrame << 8 || 0);
+    freesStart_ = dccvmm_phy_read(freeFrame << 8);
     return freeFrame;
 }
 
@@ -35,23 +35,37 @@ void os_init(void) {
 
 void os_alloc(uint32_t addr) {//TODO check for errors and exceptions
     //search for a free frame
+    if (PAGEOFFSET(addr)) {
+        printf("Invalid Alloc on address :[%x]\n", addr);
+        return;
+    }
     uint32_t freeFrame = getFreeFrame();
     //make page table point to the found frame
-    uint32_t pageDir = dccvmm_phy_read(procTable_ << 8 | currentPID_);    
+    uint32_t pageDir = dccvmm_phy_read(procTable_ << 8 | currentPID_);
     uint32_t frameOfPageTable = dccvmm_phy_read(pageDir << 8 | PTE1OFF(addr));
+    if ((frameOfPageTable & PTE_VALID) != PTE_VALID) {
+        //pointed frame is not a valid frame 
+        frameOfPageTable = getFreeFrame();
+        dccvmm_phy_write(pageDir << 8 | PTE1OFF(addr),
+                frameOfPageTable | PTE_VALID | PTE_INMEM | PTE_RW);
+    }
+
     //write on the page table the allocated Frame
-    dccvmm_phy_write(frameOfPageTable<<8 | PTE2OFF(addr), freeFrame);    
+    dccvmm_phy_write(frameOfPageTable << 8 | PTE2OFF(addr), freeFrame | PTE_VALID | PTE_INMEM | PTE_RW);
 }
 
 void os_free(uint32_t addr) {//TODO check for errors and exceptions
     //find the phy addres relative to the virtual address
-    uint32_t pdAddr = dccvmm_phy_read(procTable_<< 8 | currentPID_);
+    uint32_t pdAddr = dccvmm_phy_read(procTable_ << 8 | currentPID_);
+    printf("pageDir @[%x]\n", pdAddr);
     uint32_t ptFrame = dccvmm_phy_read(pdAddr << 8 | PTE1OFF(addr));
-    uint32_t phyAddr = dccvmm_phy_read(ptFrame << 8 | PTE2OFF(addr));
+    printf("pageTable @[%x]\n", ptFrame);
+    uint32_t phyAddr = dccvmm_phy_read(PTEFRAME(ptFrame << 8) | PTE2OFF(addr));
+    printf("freedFrame @[%x]\n", phyAddr);
     //update the page table
-    dccvmm_phy_write(ptFrame << 8 | PTE2OFF(addr), 0);
+    dccvmm_phy_write(PTEFRAME(ptFrame << 8) | PTE2OFF(addr), 0);
     //update the free list:
-    dccvmm_phy_write(phyAddr << 8, freesStart_);    
+    dccvmm_phy_write(PTEFRAME(phyAddr << 8), freesStart_);
     //	make the freed frame the freeStart and make it point to the old freeStart
     freesStart_ = phyAddr;
 
@@ -61,21 +75,26 @@ void os_swap(uint32_t pid) {
     //look for the frame of the process page table
     // if not on the memory, load from disk
     //call function to set the pagetable
-    uint32_t procPT = dccvmm_phy_read(procTable_ << 8 | pid);
-    if (procPT & PTE_INMEM) {
-        dccvmm_set_page_table(dccvmm_phy_read(procPT));
+    uint32_t processDirEntries = dccvmm_phy_read(procTable_ << 8 | pid);
+    if (processDirEntries & PTE_INMEM) {
+        dccvmm_set_page_table(dccvmm_phy_read(processDirEntries));
     } else {
-        if (procPT & PTE_VALID) {
+        if (processDirEntries & PTE_VALID) {
             //process exists but is in the disk
             //TODO
         } else {
-            procPT = getFreeFrame();
-            dccvmm_phy_write(procTable_ << 8 | pid, procPT);
+            processDirEntries = getFreeFrame();
+            dccvmm_phy_write(procTable_ << 8 | pid, processDirEntries);
         }
     }
     currentPID_ = pid;
 }
 
 uint32_t os_pagefault(uint32_t address, uint32_t perms, uint32_t pte) {
+    if ((pte & PTE_VALID) != PTE_VALID) {
+        printf("Faulty memmory access @[%x]\n", address);
+        return VM_ABORT;
+    }
 
+    // TODO: check INMEM flag - part 5
 }
